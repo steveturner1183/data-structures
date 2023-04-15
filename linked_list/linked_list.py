a69b82a9ac15850a683cad1e5965cb72506270ec
Steven Turner <81867253+steveturner1183@users.noreply.github.com>
2023-04-15 19:38:19 -0400

    Add skeleton code

---
+++ linked_list/linked_list.py
@@ -0,0 +1,105 @@
+class Node:
+    """
+    Node for use in linked list
+    """
+    def __init__(self):
+        pass
+
+
+class LinkedList:
+    """
+    Implementation of singly linked list
+    """
+    def __init__(self):
+        pass
+
+    def lenth(self):
+        """
+        Returns length of linked list
+        :return: length of linked list
+        """
+        pass
+
+    def add_front(self):
+        """
+        Add value to the front of linked list
+        :return:
+        """
+        pass
+
+    def add_back(self):
+        """
+        Add value to the back of the linked list
+        :return: None
+        """
+        pass
+
+    def insert_at_index(self, value, index):
+        """
+        Insert value at given index
+        :param value: Value to be inserted
+        :param index: Index for insertion
+        :return:
+        """
+        pass
+
+    def remove_front(self):
+        """
+        Remove value at the front of the linked list
+        :return:
+        """
+        pass
+
+    def remove_back(self):
+        """
+        Remove value at the back of the linked list
+        :return: None
+        """
+        pass
+
+    def remove_at_index(self, index):
+        """
+        Remove value at a given index
+        :param index: index value is to be removed at
+        :return: None
+        """
+        pass
+
+    def get_front(self):
+        """
+        Return value at the front of the linked list
+        :return: Value at front
+        """
+        pass
+
+    def get_back(self):
+        """
+        return value at the back of linked list
+        :return: Value at back
+        """
+        pass
+
+    def remove_value(self, value):
+        """
+        Removes given value from the list
+        :param value: Value to be removed
+        :return: None
+        """
+        pass
+
+    def count(self, value):
+        """
+        Returns count of nodes that match given value
+        :param value: Value to be counted
+        :return: Count of value
+        """
+        pass
+
+    def slice(self, start, size):
+        """
+        Returns new linked list with value only from start to size
+        :param start: Slice start
+        :param size: Amount to slice
+        :return: New sliced linked list
+        """
+        pass
---
+++ linked_list/ll_test.py
@@ -0,0 +1,43 @@
+import unittest
+from linked_list import LinkedList
+
+
+class TestQue(unittest.TestCase):
+    def test_lenth(self):
+        pass
+
+    def test_add_front(self):
+        pass
+
+    def test_add_back(self):
+        pass
+
+    def test_insert_at_index(self, value, index):
+        pass
+
+    def test_remove_front(self):
+        pass
+
+    def test_remove_back(self):
+        pass
+
+    def test_remove_at_index(self, index):
+        pass
+
+    def test_get_front(self):
+        pass
+
+    def test_get_back(self):
+        pass
+
+    def test_remove_value(self, value):
+        pass
+
+    def test_count(self, value):
+        pass
+
+    def test_slice(self, start, size):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
