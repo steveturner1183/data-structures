66091bdccc582fa8dba6280f3f68d8f3440f5760
Steven Turner <81867253+steveturner1183@users.noreply.github.com>
2023-04-15 12:27:40 -0400

    Update delete function test

--- dynamicArray/test.py
+++ dynamicArray/test.py
@@ -3,9 +3,6 @@ from dynamic_array import DynamicArray
 
 class TestDynamicArray(unittest.TestCase):
     def test_set(self):
-        """
-        Tests the set function
-        """
         test_da = DynamicArray(initial_capacity=4)
         test_da.set(1, 1)
         expected = [0, 1, 0, 0]
@@ -13,9 +10,6 @@ class TestDynamicArray(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_get_index(self):
-        """
-        Test the get function
-        """
         test_da = DynamicArray(initial_capacity=4)
         test_da.set(1, 1)
         expected = 1
@@ -57,6 +51,16 @@ class TestDynamicArray(unittest.TestCase):
         test_da.append(3)
         expected = [1, 2, 0, 0]
         test_da.remove()
+        actual = test_da.get_arr()
+        self.assertEqual(expected, actual)
+
+    def test_delete(self):
+        test_da = DynamicArray(initial_capacity=4)
+        test_da.append(1)
+        test_da.append(2)
+        test_da.append(3)
+        expected = [1, 3, 0, 0]
+        test_da.delete(1)
         actual = test_da.get_arr()
         self.assertEqual(expected, actual)
 
