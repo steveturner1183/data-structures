4330e57139d8ff5e1c1f81e21408a6aadc38a498
Steven Turner <81867253+steveturner1183@users.noreply.github.com>
2023-04-15 11:17:56 -0400

    Add outline skeleton code

---
+++ dynamicArray/dynamic_array.py
@@ -0,0 +1,76 @@
+class DynamicArray:
+    """
+    Implementation of dynamic array with add, insert, remove, and delete
+    methods
+    """
+    def __init__(self, initial_capacity):
+        self._size = 0
+        self._capacity = initial_capacity
+        self._data = [0] * initial_capacity
+
+    ###########################################################################
+    # Private Helper Functions
+    ###########################################################################
+
+    def _copy_contents(self, arr):
+        """
+        Copies the contents of self._data to a new array
+        :param arr: Empty array
+        :return: Array with copied contents
+        """
+        pass
+
+    def _grow_array(self):
+        """
+        Creates a new array of double the size of capacity, and copies over
+        data from self._data array
+        :return: None
+        """
+        pass
+
+    def get(self, index):
+        """
+        Retrieve element at given index
+        :param index: Index to have element retrieved
+        :return: Element at given index
+        """
+        pass
+
+    def set(self, index):
+        """
+        Change value at given index
+        :param index: Index to have element changed
+        :return:
+        """
+        pass
+
+    def add(self):
+        """
+        Add element to the end of array
+        :return: None
+        """
+        pass
+
+    def insert(self, index):
+        """
+        Insert element at a given index. Shift elements after given index
+        forward
+        :param index:
+        :return: None
+        """
+        pass
+
+    def remove(self):
+        """
+        Remove element from the end of array
+        :return: None
+        """
+        pass
+
+    def delete(self, index):
+        """
+        Delete element at given index
+        :param index:
+        :return: None
+        """
+        pass
