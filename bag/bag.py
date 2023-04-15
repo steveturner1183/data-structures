7577d611fd566a53642504c9c4aec7e18814ab7c
Steven Turner <81867253+steveturner1183@users.noreply.github.com>
2023-04-15 18:38:38 -0400

    implement clear

--- bag/bag.py
+++ bag/bag.py
@@ -2,8 +2,8 @@ from dynamic_array.dynamic_array import DynamicArray
 
 class Bag:
     """
-    Bag ADT with add, remove, clear, count, size, display, and equal check
-    build off existing dynamic array object
+    Bag ADT with add, remove, clear, count, size, display
+    Built off existing dynamic array object
     """
     def __init__(self, initial_capacity):
         self._bag = DynamicArray(initial_capacity=initial_capacity)
