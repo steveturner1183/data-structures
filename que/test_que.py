ff6afefbec56181937b21ad8a0e0897fbb40ade4
Steven Turner <81867253+steveturner1183@users.noreply.github.com>
2023-04-15 18:52:47 -0400

    Add deque test

--- que/test_que.py
+++ que/test_que.py
@@ -10,5 +10,13 @@ class TestDynamicArray(unittest.TestCase):
         actual = test_que.get_que()
         self.assertEqual(expected, actual)
 
+    def test_deque(self):
+        test_que = Que(initial_capacity=4)
+        test_que.enque(1)
+        test_que.enque(2)
+        expected = 1
+        actual = test_que.deque()
+        self.assertEqual(expected, actual)
+
 if __name__ == "__main__":
     unittest.main()
