1a28cc9b92f8664fa08c646a726a0cc79d389a72
Steven Turner <81867253+steveturner1183@users.noreply.github.com>
2023-04-15 19:01:54 -0400

    Add pop/push test

--- stack/test_stack.py
+++ stack/test_stack.py
@@ -3,10 +3,23 @@ from stack import Stack
 
 class TestQue(unittest.TestCase):
     def test_pop(self):
-        pass
+        test_stack = Stack(initial_capacity=4)
+        test_stack.push(1)
+        test_stack.push(2)
+        test_stack.push(3)
+        expected = 2
+        test_stack.pop()
+        actual = test_stack.pop()
+        self.assertEqual(expected, actual)
 
     def test_push(self):
-        pass
+        test_stack = Stack(initial_capacity=4)
+        test_stack.push(1)
+        test_stack.push(2)
+        test_stack.push(3)
+        expected = 3
+        actual = test_stack.pop()
+        self.assertEqual(expected, actual)
 
 if __name__ == "__main__":
     unittest.main()
