bd61c768b2d24e66c8d666e9f75d6765aaa4b117
Steven Turner <81867253+steveturner1183@users.noreply.github.com>
2023-04-15 18:18:37 -0400

    add tet for remove item

--- bag/bag_test.py
+++ bag/bag_test.py
@@ -2,7 +2,16 @@ import unittest
 from bag import Bag
 
 class TestDynamicArray(unittest.TestCase):
-    pass
+    def test_remove(self, value):
+        test_bag = Bag(initial_capacity=4)
+        test_bag.add(3)
+        test_bag.add(1)
+        test_bag.add(2)
+        test_bag.remove(1)
+        expected = [3, 2, 0, 0]
+        actual = test_bag.get_bag()
+        self.assertEqual(expected, actual)
+
 
 if __name__ == "__main__":
     unittest.main()
